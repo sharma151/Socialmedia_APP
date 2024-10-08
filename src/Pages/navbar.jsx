434b302c3c3d798dc89f98b311049b244bd19c7b
sharma151 <sharmasaurav150503@gmail.com>
2024-10-08 13:21:21 +0545

    getusernamefucntionalityadded

--- src/Pages/navbar.jsx
+++ src/Pages/navbar.jsx
@@ -1,10 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import axios from "axios";
 import "../Pages/Navbar.scss";
 
 const Navbar = () => {
+  const [username, setUsername] = useState("");
+
+  const handleInputChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const fetchUserProfile = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/v1/social-media/profile/u/${username}`
+      );
+      console.log("User Profile Data:", response.data);
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
+
   return (
     <nav>
-      <ul className="flex justify-center gap-8">
+      <ul>
+        <input
+          type="text"
+          value={username}
+          onChange={handleInputChange}
+          placeholder="Enter username"
+        />
+        <button onClick={fetchUserProfile}>
+          <IoSearch  size={18} />
+        </button>
         <li>
           <Link to="/login">Login</Link>
         </li>
