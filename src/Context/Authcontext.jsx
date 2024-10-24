381f0c9fe4aba84b931c0dcc3ec1be23ec932f82
sharma151 <sharmasaurav150503@gmail.com>
2024-10-24 14:54:27 +0545

    Authcontextadded

---
+++ src/Context/Authcontext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("AccessToken");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
