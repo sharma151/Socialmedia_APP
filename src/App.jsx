999596940fef591d812d60b9acd2480a0a1105a9
sharma151 <sharmasaurav150503@gmail.com>
2024-10-24 16:43:35 +0545

    routes-syntax-changed-to-objecttype

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Navbar from "./Pages/navbar";
+
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Userpost from "./Components/Userpost";
 import ProfilePage from "./Pages/ProfilePage";
 import ProfileByUsername from "./Components/ProfilebyUsername";
+import { AuthProvider } from "./Context/Authcontext";
+
+const routes = [
+  { path: "/", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/home", element: <Home /> },
+  { path: "/profile/:username", element: <ProfileByUsername /> },
+  { path: "/posts", element: <Userpost /> },
+  { path: "/profile-page", element: <ProfilePage /> },
+];
 
 function App() {
-  const isAuthenticated = !!localStorage.getItem("AccessToken");
-
   return (
-    <Router>
-      <div className="App">
-        {/* <Navbar /> */}
-        <Routes>
-          {/* <Route
-            path="/"
-            element={
-              isAuthenticated ? (
-                <Home />
-              ) : (
-                <p>Please login to access the home page.</p>
-              )
-            }
-          /> */}
-
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:username" element={<ProfileByUsername />} />
-          <Route path="/Posts" element={<Userpost />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/profile-page" element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {routes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
