e28dd4d87d7e0d9360861f68eb0c637ee79fee04
sharma151 <sharmasaurav150503@gmail.com>
2024-10-29 11:19:59 +0545

    contextupdated

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,33 @@ import ProfileByUsername from "./Components/ProfilebyUsername";
 import { AuthProvider } from "./Context/Authcontext";
 import Layout from "./Layouts/Layout";
 import ProtectedRoute from "./Components/ProtectedRoute";
+import { UpdateProfileContextProvider } from "./Context/UpdateProfileContext";
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+      <UpdateProfileContextProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-            <Route element={<ProtectedRoute />}>
-              <Route element={<Layout />}>
-                <Route path="/home" element={<Home />} />
-                <Route
-                  path="/profile/:username"
-                  element={<ProfileByUsername />}
-                />
-                <Route path="/posts" element={<Userpost />} />
-                <Route path="/profile-page" element={<ProfilePage />} />
+              <Route element={<ProtectedRoute />}>
+                <Route element={<Layout />}>
+                  <Route path="/home" element={<Home />} />
+                  <Route
+                    path="/profile/:username"
+                    element={<ProfileByUsername />}
+                  />
+                  <Route path="/posts" element={<Userpost />} />
+                  <Route path="/profile-page" element={<ProfilePage />} />
+                </Route>
               </Route>
-            </Route>
-          </Routes>
-        </div>
-      </Router>
+            </Routes>
+          </div>
+        </Router>
+      </UpdateProfileContextProvider>
     </AuthProvider>
   );
 }
