0f0a6ca64031de43a2e8cbe0dd7001df9ffa7f6d
sharma151 <sharmasaurav150503@gmail.com>
2024-10-30 10:50:26 +0545

    BookmarkPage_added

---
+++ src/Pages/Bookmarks.jsx
@@ -0,0 +1,11 @@
+// import Userpost from "../Components/Userpost";
+const Bookmarks = () => {
+  return (
+    <div>
+      Bookmarks
+      {/* <Userpost /> */}
+    </div>
+  );
+};
+
+export default Bookmarks;
