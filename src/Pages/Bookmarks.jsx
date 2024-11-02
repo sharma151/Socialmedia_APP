994276b388b573fb4c31139e717c892d60494d48
sharma151 <sharmasaurav150503@gmail.com>
2024-11-02 17:45:56 +0545

    userpostimported

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -1,9 +1,19 @@
-// import Userpost from "../Components/Userpost";
+import Userpost from "../Components/Userpost";
+import { useContext } from "react";
+import { AuthContext } from "../Context/Authcontext";
+import "../Styles/Bookmarks.scss";
+
 const Bookmarks = () => {
+  const { bookmarks } = useContext(AuthContext);
+  console.log(bookmarks);
   return (
     <div>
-      Bookmarks
-      {/* <Userpost /> */}
+      <h6>Bookmarks</h6>
+      {bookmarks ? (
+        <Userpost posts={bookmarks} />
+      ) : (
+        <p>Error Fetching Bookmarks</p>
+      )}
     </div>
   );
 };
