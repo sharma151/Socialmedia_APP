e6331c8abb636d7cce27beeb9d6e875b05b20c52
sharma151 <sharmasaurav150503@gmail.com>
2024-11-12 14:19:15 +0545

    SetUsernamecontext-added to homepage

---
+++ src/Context/Setusername.jsx
@@ -0,0 +1,12 @@
+import { useState, createContext } from "react";
+
+export const UsernameContext = createContext();
+
+export const Setusername = ({ children }) => {
+  const [UserName, SetUserName] = useState();
+  return (
+    <UsernameContext.Provider value={{ UserName, SetUserName }}>
+      {children}
+    </UsernameContext.Provider>
+  );
+};
