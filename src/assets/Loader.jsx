860bd664cd67571da22e5dbefc6ce513c36c2459
sharma151 <sharmasaurav150503@gmail.com>
2024-11-15 11:34:25 +0545

    Loader added

---
+++ src/assets/Loader.jsx
@@ -0,0 +1,20 @@
+import { BallTriangle } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <div>
+      <BallTriangle
+        height={70}
+        width={70}
+        radius={5}
+        color="#243642"
+        ariaLabel="ball-triangle-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Loader;
