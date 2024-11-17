ff58c7d95d4d2f0bbf98027aabb4b2d206dcea61
sharma151 <sharmasaurav150503@gmail.com>
2024-11-17 19:11:56 +0545

    likedislike-btn-added

--- src/Components/Userpost.jsx
+++ src/Components/Userpost.jsx
@@ -1,21 +1,16 @@
-import {
-  useState,
-  useEffect,
-  useContext,
-  useReducer,
-  useMemo,
-  memo,
-} from "react";
-// import axios from "../services/Api";
-import { MdDelete } from "react-icons/md";
-import { toast } from "react-toastify";
-import { UpdatedataContext } from "../Context/UpdateProfileContext";
 import { PiBookmarkSimpleBold, PiBookmarkSimpleFill } from "react-icons/pi";
+import { useState, useContext, useEffect } from "react";
+import { UpdatedataContext } from "../Context/UpdateProfileContext";
+import { HandleDeletePost, handleLikePost } from "../services/Handleapi";
 import { AuthContext } from "../Context/Authcontext";
+import { MdDelete } from "react-icons/md";
+import { BiLike, BiSolidLike } from "react-icons/bi";
+import { toast } from "react-toastify";
 import "../Styles/Post.scss";
+import { handleFetchallPost } from "../services/Handleapi";
 
-const DELETE_POST = "DELETE_POST";
-const SET_POSTS = "SET_POSTS";
+// const DELETE_POST = "DELETE_POST";
+// const SET_POSTS = "SET_POSTS";
 
 const formatDate = (dateString) => {
   const options = { year: "numeric", month: "long", day: "numeric" };
@@ -24,50 +19,70 @@ const formatDate = (dateString) => {
 };
 
 const Posts = ({ className, posts, onUpdate }) => {
-  const [loading, setLoading] = useState();
   const { bookmarks, setBookmarks } = useContext(AuthContext);
-
   const { UserprofileData } = useContext(UpdatedataContext);
-  const [state, dispatch] = useReducer(reducer, { posts: [] });
+  // const [state, dispatch] = useReducer(reducer, { posts: [] });
+  const [userposts, setUserposts] = useState(posts);
 
-  // const handleDeletePost = async (_id) => {
-  //   // reducer
-  //   try {
-  //     const response = await axios.delete(`/social-media/posts/${_id}`);
-  //     if (response.status === 200) {
-  //       toast.success("Post deleted successfully");
-  //       if (onUpdate) {
-  //         onUpdate();
-  //       }
-  //     }
-  //   } catch (error) {
-  //     console.error("Error deleting post:", error);
-  //     toast.error("Failed to delete the post.");
-  //   }
-  // };
-
-  const deletePost = (postId) => {
-    dispatch({ type: DELETE_POST, payload: postId });
+  const Handledeletepostsubmit = async (e, _id) => {
+    e.preventDefault();
+    try {
+      const response = await HandleDeletePost(_id);
+      if (response === 200) {
+        toast.success("Post deleted successfully");
+        if (onUpdate) {
+          onUpdate();
+        }
+      }
+    } catch (error) {
+      console.error("Error deleting post:", error);
+      toast.error("Failed to delete the post.");
+    }
   };
 
-  function reducer(state, action) {
-    switch (action.type) {
-      case DELETE_POST:
-        toast.success("post deleted");
-        return {
-          ...state,
-          posts: state.posts.filter((post) => post._id !== action.payload),
-        };
-
-      case SET_POSTS:
-        return {
-          ...state,
-          posts: action.payload,
-        };
-      default:
-        return state;
+  const handleLikePostSubmit = async (_id) => {
+    try {
+      const response = await handleLikePost(_id);
+      const updatedposts = userposts.map((post) => {
+        if (post._id === _id) {
+          post.isLiked = response.isLiked;
+          if (response.isLiked) {
+            post.likes++;
+          } else {
+            post.likes--;
+          }
+          return post;
+        }
+        return post;
+      });
+      setUserposts(updatedposts);
+    } catch (error) {
+      console.error("Error liking post", error);
     }
-  }
+  };
+
+  // const deletePost = (postId) => {
+  //   dispatch({ type: DELETE_POST, payload: postId });
+  // };
+
+  // function reducer(state, action) {
+  //   switch (action.type) {
+  //     case DELETE_POST:
+  //       toast.success("post deleted");
+  //       return {
+  //         ...state,
+  //         posts: state.posts.filter((post) => post._id !== action.payload),
+  //       };
+
+  //     case SET_POSTS:
+  //       return {
+  //         ...state,
+  //         posts: action.payload,
+  //       };
+  //     default:
+  //       return state;
+  //   }
+  // }
 
   const handleBookmarkClick = (post) => {
     const isBookmarked = bookmarks.some((item) => item._id === post._id);
@@ -81,18 +96,15 @@ const Posts = ({ className, posts, onUpdate }) => {
     }
   };
 
-  useEffect(() => {
-    dispatch({ type: SET_POSTS, payload: posts });
-  }, [posts]);
+  // useEffect(() => {
+  //   dispatch({ type: SET_POSTS, payload: posts });
+  // }, [posts]);
 
-  if (loading) {
-    return <p>Loading posts...</p>;
-  }
   return (
     <div className={`posts ${className} increment decrement count  `}>
       {/* <h2>All Posts</h2> */}
       <div className="posts-list">
-        {state.posts.map((post) => (
+        {userposts.map((post) => (
           <div key={post._id} className="post-item">
             {post?.author?.account?.avatar?.url && (
               <img
@@ -109,12 +121,12 @@ const Posts = ({ className, posts, onUpdate }) => {
               <p className="LastName">{post?.author?.lastName}</p>
               <p className="createdAt">{formatDate(post?.createdAt)}</p>
             </div>
-            {UserprofileData._id === post?.author?._id && (
+            {UserprofileData._id === userposts?.author?._id && (
               <>
                 <button
                   className="delete-btn"
-                  onClick={() => deletePost(post?._id)}
-                  // onClick={() => handleDeletePost(post?._id)}
+                  // onClick={() => deletePost(post?._id)}
+                  onClick={(e) => Handledeletepostsubmit(e, userposts?._id)}
                 >
                   <MdDelete size={25} />
                 </button>
@@ -130,16 +142,29 @@ const Posts = ({ className, posts, onUpdate }) => {
                 />
               )}
             </div>
-            <button
-              className="bookmark"
-              onClick={() => handleBookmarkClick(post)}
-            >
-              {bookmarks.some((bookmark) => bookmark._id === post._id) ? (
-                <PiBookmarkSimpleFill size={27} />
-              ) : (
-                <PiBookmarkSimpleBold size={27} />
-              )}
-            </button>
+            <div className="Post-buttons">
+              <button
+                onClick={() => handleLikePostSubmit(post?._id)}
+                className="Like-dislike"
+              >
+                {post?.isLiked ? (
+                  <BiSolidLike size={27} />
+                ) : (
+                  <BiLike size={27} />
+                )}
+                {post.likes}
+              </button>
+              <button
+                className="bookmark"
+                onClick={() => handleBookmarkClick(post)}
+              >
+                {bookmarks.some((bookmark) => bookmark._id === post._id) ? (
+                  <PiBookmarkSimpleFill size={27} />
+                ) : (
+                  <PiBookmarkSimpleBold size={27} />
+                )}
+              </button>
+            </div>
           </div>
         ))}
       </div>
