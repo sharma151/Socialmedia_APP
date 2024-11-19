9ed2b94c5d6c480840ef08a672f4d7b96d257d67
sharma151 <sharmasaurav150503@gmail.com>
2024-11-19 09:57:31 +0545

    bookmarks added

--- src/Pages/Bookmarks.jsx
+++ src/Pages/Bookmarks.jsx
@@ -1,20 +1,36 @@
+// import { AuthContext } from "../Context/Authcontext";
+// import { useContext } from "react";
+import { useState, useEffect } from "react";
 import Userpost from "../Components/Userpost";
-import { useContext } from "react";
-import { AuthContext } from "../Context/Authcontext";
 import "../Styles/Bookmarks.scss";
+import { toast } from "react-toastify";
+import { handleFetchBookmarks } from "../services/Handleapi";
 
 const Bookmarks = () => {
-  const { bookmarks } = useContext(AuthContext);
-  console.log(bookmarks);
+  // const { bookmarks } = useContext(AuthContext);
+
+  const [bookmarkedPosts, setBookmarkedPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchBookmarks = async () => {
+      try {
+        const response = await handleFetchBookmarks();
+        setBookmarkedPosts(response);
+
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching bookmarks:", error);
+        toast.error("Failed to fetch bookmarks.");
+      }
+    };
+
+    fetchBookmarks();
+  }, []);
+
   return (
-    <div>
-      <h6>Bookmarks</h6>
-      {bookmarks ? (
-        <Userpost posts={bookmarks} />
-      ) : (
-        <p>Error Fetching Bookmarks</p>
-      )}
-    </div>
+    <>
+      <Userpost posts={bookmarkedPosts} />
+    </>
   );
 };
 
