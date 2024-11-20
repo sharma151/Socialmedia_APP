93e5fd82a4f9385af06cd12c382a5272b43bc3d0
sharma151 <sharmasaurav150503@gmail.com>
2024-11-20 10:41:11 +0545

    minorchanges

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Navbar";
+import { Setusername } from "../Context/Setusername";
 
 function Layout() {
   return (
     <div>
-      <Navbar />
-      <div className="content">
-        <Outlet />
-      </div>
+      <Setusername>
+        <Navbar />
+        <div className="content">
+          <Outlet />
+        </div>
+      </Setusername>
     </div>
   );
 }
