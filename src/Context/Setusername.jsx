f002d911f3203939a64ff724d0814a1b53a8c578
sharma151 <sharmasaurav150503@gmail.com>
2024-11-21 10:04:31 +0545

    minorchanges

--- src/Context/Setusername.jsx
+++ src/Context/Setusername.jsx
@@ -3,7 +3,8 @@ import { useState, createContext } from "react";
 export const UsernameContext = createContext();
 
 export const Setusername = ({ children }) => {
-  const [UserName, SetUserName] = useState();
+  const [UserName, SetUserName] = useState("");
+
   return (
     <UsernameContext.Provider value={{ UserName, SetUserName }}>
       {children}
