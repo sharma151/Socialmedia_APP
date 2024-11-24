e14fb63eea57df1eedbf4190423f1e71e071fd2c
sharma151 <sharmasaurav150503@gmail.com>
2024-11-24 21:52:14 +0545

    minrochanges

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import { ToastContainer } from "react-toastify";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
