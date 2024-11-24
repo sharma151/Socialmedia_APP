a90b99e5b05978ef735b7be2e9e5d42c8972a0d2
sharma151 <sharmasaurav150503@gmail.com>
2024-11-24 21:51:59 +0545

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UpdateProfileContextProvider } from "./Context/UpdateProfileContext";
+import { AuthProvider } from "./Context/Authcontext";
+import ProfileByUsername from "./Components/ProfilebyUsername";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import ProfilePage from "./Pages/ProfilePage";
+import Bookmarks from "./Pages/Bookmarks";
 import Register from "./Pages/Register";
+import Userpost from "./Components/Userpost";
+import Layout from "./Layouts/Layout";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
-import Userpost from "./Components/Userpost";
-import ProfilePage from "./Pages/ProfilePage";
-import ProfileByUsername from "./Components/ProfilebyUsername";
-import { AuthProvider } from "./Context/Authcontext";
-import Layout from "./Layouts/Layout";
-import ProtectedRoute from "./Components/ProtectedRoute";
-import Bookmarks from "./Pages/Bookmarks";
-import { UpdateProfileContextProvider } from "./Context/UpdateProfileContext";
+import "./App.css";
 
 function App() {
   return (
@@ -19,9 +19,7 @@ function App() {
         <div className="App">
           <Routes>
             <Route path="/login" element={<Login />} />
-
             <Route path="/register" element={<Register />} />
-
             <Route element={<ProtectedRoute />}>
               <Route
                 element={
@@ -31,8 +29,8 @@ function App() {
                 }
               >
                 <Route path="/" element={<Home />} />
-
                 <Route path="/Bookmarks" element={<Bookmarks />} />
+                {/* <Route path="/Bookmarks-withoutApi" element={<Bookmarks />} /> */}
                 <Route
                   path="/profile/:username"
                   element={<ProfileByUsername />}
