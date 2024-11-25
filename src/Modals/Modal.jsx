be29e84a7efe9e0a3e60626e617c1a45baa0312a
sharma151 <sharmasaurav150503@gmail.com>
2024-11-25 11:27:32 +0545

    modal-template added

---
+++ src/Modals/Modal.jsx
@@ -0,0 +1,24 @@
+import "../Styles/Modal.scss";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div
+        className="modal-content"
+        onClick={(e) => {
+          e.stopPropagation(), onClose;
+        }}
+      >
+        <button className="modal-close" onClick={onClose}>
+          <IoIosCloseCircleOutline size={30}/>
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
