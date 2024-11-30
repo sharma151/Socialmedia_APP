01e6ddf444f95174c514123ea353e4291ce53fb7
sharma151 <sharmasaurav150503@gmail.com>
2024-11-30 19:41:47 +0545

    minorchanges

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,23 +1,22 @@
-import { useState, useEffect } from "react";
-import axios from "../services/Api";
-import "../Styles/ProfilePage.scss";
+import { handleFetchmyPost, handleFetchMyprofile } from "../services/Handleapi";
 import { FaBirthdayCake, FaUserAlt } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
-import { IoCall } from "react-icons/io5";
-import { MdModeEdit } from "react-icons/md";
-import Createpost from "../Components/Createpost";
-import UpdateCoverPage from "../Components/Updatecoverpicture";
-import Updateavatar from "../Components/Updateavatar";
-import ProfileUpdate from "../Components/ProfileUpdate";
-import Userpost from "../Components/Userpost";
-import { IoIosMail } from "react-icons/io";
+import { useState, useEffect } from "react";
 import { UpdatedataContext } from "../Context/UpdateProfileContext";
+import { FaLocationDot } from "react-icons/fa6";
 import { useContext } from "react";
-
+import { IoIosMail } from "react-icons/io";
+import { IoCall } from "react-icons/io5";
 import { toast } from "react-toastify";
+import UpdateCoverPage from "../Components/Updatecoverpicture";
+import ProfileUpdate from "../Components/ProfileUpdate";
+import Updateavatar from "../Components/Updateavatar";
+import Createpost from "../Components/Createpost";
+import Userpost from "../Components/Userpost";
+import Loader from "../assets/Loader";
+import "../Styles/ProfilePage.scss";
 
 const ProfilePage = () => {
-  const { UserprofileData, setUserProfileData } = useContext(UpdatedataContext);
+  const { UserprofileData } = useContext(UpdatedataContext);
   const [profileImages, setprofileImages] = useState({
     avatar: "",
     coverImage: "",
@@ -26,112 +25,141 @@ const ProfilePage = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [showModal, setShowModal] = useState(false);
   const closeModal = () => setShowModal(false);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
 
   const fetchProfile = async () => {
     try {
-      const response = await axios.get("/social-media/profile");
-      setprofileImages(response?.data?.data);
+      const response = await handleFetchMyprofile();
+      setprofileImages(response);
     } catch (error) {
-      console.error("Error fetching profile:", error);
+      toast.error("Error fetching profile:", error);
       setErrorMessage("Error fetching profile data.");
     }
   };
 
   const fetchMyPosts = async () => {
     try {
-      setLoading(true);
-      const response = await axios.get(
-        `/social-media/posts/get/my?page=1&limit=100`
-      );
-      const sortedPosts = response?.data?.data?.posts.sort(
+      const response = await handleFetchmyPost();
+      const sortedPosts = response.sort(
         (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
       );
       setPosts(sortedPosts);
-      setLoading(false);
     } catch (err) {
       toast("Failed to fetch posts.", err);
-      setLoading(false);
     }
   };
 
+  const fetchAllData = async () => {
+    setLoading(true);
+    await Promise.all([fetchProfile(), fetchMyPosts()]);
+  };
+
   useEffect(() => {
-    fetchProfile();
-    fetchMyPosts();
+    fetchAllData();
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className="ProfilePage">
       {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
 
-      <div className="Images">
-        {profileImages?.coverImage?.url && (
-          <img
-            key={profileImages?.id}
-            src={profileImages?.coverImage?.url}
-            alt={profileImages?.coverImage}
-            className="coverimage"
+      {loading ? (
+        <Loader className="Profilepage-loader" />
+      ) : (
+        <>
+          <div className="Images">
+            {profileImages?.coverImage?.url && (
+              <img
+                key={profileImages?.id}
+                src={profileImages?.coverImage?.url}
+                alt={profileImages?.coverImage}
+                className="coverimage"
+              />
+            )}
+
+            {profileImages?.account?.avatar?.url && (
+              <img
+                src={profileImages?.account?.avatar?.url}
+                alt={profileImages?.posts?.avatar}
+                className="avatar"
+              />
+            )}
+
+            <div className="Name">
+              <p> {UserprofileData?.firstName}</p>
+              <p> {UserprofileData?.lastName}</p>
+              <h1>{UserprofileData?.account?.username}</h1>
+            </div>
+          </div>
+
+          <div className="profile-info">
+            <h4>Profile Details</h4>
+            <div className="data">
+              <div className="Bio">
+                <span>
+                  <FaUserAlt size={18} className="Bio-icon" />
+                </span>{" "}
+                {UserprofileData?.bio}
+              </div>
+              <div className="DOB">
+                {/* <strong>Date of Birth:</strong> */}
+                <span className="birthday-icon">
+                  <FaBirthdayCake size={18} />
+                </span>{" "}
+                {new Date(UserprofileData?.dob).toLocaleDateString()}
+              </div>
+              <div className="Location">
+                <span className="location-icon">
+                  <FaLocationDot size={18} />{" "}
+                </span>
+                {UserprofileData?.location}
+              </div>
+              <div className="Phonenumber">
+                <span className="phone">
+                  <IoCall size={18} />{" "}
+                </span>
+                {UserprofileData?.countryCode} {UserprofileData?.phoneNumber}
+              </div>
+              <div className="Mail">
+                <span className="emailicons">
+                  <IoIosMail size={18} />
+                </span>{" "}
+                {UserprofileData?.account?.email}
+              </div>
+              <button
+                className="Updateprofile"
+                onClick={() => setShowModal(true)}
+              >
+                {/* <MdModeEdit size={25} /> */}
+                <p>Edit profile</p>
+              </button>
+            </div>
+          </div>
+
+          <Createpost
+            className="profilepage-createpost"
+            onUpdate={() => fetchMyPosts()}
           />
-        )}
 
-        {profileImages?.account?.avatar?.url && (
-          <img
-            src={profileImages?.account?.avatar?.url}
-            alt={profileImages?.posts?.avatar}
-            className="avatar"
+          <Userpost
+            className="Getmyposts"
+            onUpdate={() => fetchMyPosts()}
+            posts={posts}
           />
-        )}
-
-        <div className="Name">
-          <p> {UserprofileData?.firstName}</p>
-          <p> {UserprofileData?.lastName}</p>
-          <h1>{UserprofileData?.account?.username}</h1>
-        </div>
-        <button className="Updateprofile" onClick={() => setShowModal(true)}>
-          {" "}
-          <MdModeEdit size={25} /> <p>Edit profile</p>
-        </button>
-      </div>
-
-      <div className="profile-info">
-        <div className="data">
-          <p className="Bio">
-            <FaUserAlt  /> {UserprofileData?.bio}
-          </p>
-          <p className="DOB">
-            {/* <strong>Date of Birth:</strong> */}
-            <FaBirthdayCake  />{" "}
-            {new Date(UserprofileData?.dob).toLocaleDateString()}
-          </p>
-          <p className="Location">
-            <FaLocationDot /> {UserprofileData?.location}
-          </p>
-          <p className="Phonenumber">
-            <IoCall  /> {UserprofileData?.countryCode}{" "}
-            {UserprofileData?.phoneNumber}
-          </p>
-          <p className="Mail">
-            <IoIosMail  size={15}className="emailicons" /> {UserprofileData?.account?.email}
-          </p>
-        </div>
-      </div>
-
-      <Createpost className="createpost" onUpdate={() => fetchMyPosts()} />
-
-      <Userpost
-        className="myposts"
-        onUpdate={() => fetchMyPosts()}
-        posts={posts}
-      />
-      <UpdateCoverPage onUpdate={() => fetchProfile()} />
-      <Updateavatar onUpdate={() => fetchProfile()} />
-      {showModal && (
-        <ProfileUpdate
-          // currentProfile={profile}
-          closeModal={closeModal}
-          onUpdate={() => fetchProfile()}
-        />
+          <UpdateCoverPage onUpdate={() => fetchProfile()} />
+          <Updateavatar onUpdate={() => fetchProfile()} />
+          {showModal && (
+            <ProfileUpdate
+              // currentProfile={profile}
+              closeModal={closeModal}
+              onUpdate={() => fetchProfile()}
+            />
+          )}
+        </>
       )}
     </div>
   );
