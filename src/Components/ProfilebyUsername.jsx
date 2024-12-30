ae5b1a086842959934876d2e5b81490ed05809c9
sharma151 <sharmasaurav150503@gmail.com>
2024-12-30 18:41:21 +0545

    responsived added

--- src/Components/ProfilebyUsername.jsx
+++ src/Components/ProfilebyUsername.jsx
@@ -1,5 +1,6 @@
 import { FaBirthdayCake, FaUserAlt } from "react-icons/fa";
 import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaLocationDot } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
 import { IoCall } from "react-icons/io5";
@@ -17,22 +18,38 @@ const GetProfileByUsername = () => {
   const { SetUserName } = useContext(UsernameContext);
   const [userData, setUserData] = useState(null);
   const [userNamepost, setUserNamePost] = useState([]);
-
+  const navigate = useNavigate();
   const fetchUserData = async () => {
     try {
       const response = await handleFetchuserData(username);
-      setUserData(response);
+      if (response) {
+        setUserData(response);
+      } else {
+        navigate("/usernotfound");
+      }
     } catch (error) {
-      toast.error("Error fetching user data:", error);
+      if (error?.response?.data?.statusCode === 404) {
+        navigate("/usernotfound");
+      } else {
+        toast.error(`Error fetching user data: ${error.message}`);
+      }
     }
   };
 
   const fetchpostByUsername = async () => {
     try {
       const response = await handleFetchpostByusername(username);
-      setUserNamePost(response);
+      if (response) {
+        setUserNamePost(response);
+      } else {
+        navigate("/usernotfound");
+      }
     } catch (error) {
-      toast.error("Error fetching user data:", error);
+      if (error?.response?.data?.statusCode === 404) {
+        navigate("/usernotfound");
+      } else {
+        toast.error("Error fetching user data:", error);
+      }
     }
   };
 
@@ -42,53 +59,82 @@ const GetProfileByUsername = () => {
   }, [username]);
 
   return (
-    <div className="ProfilePage">
-      <div className="Images">
-        {userData?.data?.coverImage?.url && (
-          <img
-            // key={userData?.id}
-            src={userData?.data?.coverImage?.url}
-            alt={userData?.data?.coverImage}
+    <>
+      <div className="ProfilePage">
+        <div className="Images">
+          <div
             className="coverimage"
-          />
-        )}
+            style={{
+              backgroundImage: `url(${userData?.data?.coverImage?.url || ""})`,
+              backgroundSize: "100% 100%",
+              overflow: "hidden",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
 
-        {userData?.data?.account?.avatar?.url && (
-          <img
-            src={userData?.data?.account?.avatar?.url}
-            alt={userData?.data?.account?.avatar}
-            className="avatar"
-          />
-        )}
+          <div className="avatar_name">
+            <div
+              className="avatar"
+              style={{
+                backgroundImage: `url(${
+                  userData?.data?.account?.avatar?.url || ""
+                })`,
+                backgroundSize: "100% 100%",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            <div className="Name">
+              <p>
+                {" "}
+                {userData?.data?.firstName}
+                {userData?.data?.lastName}
+              </p>
 
-        <div className="Name">
-          <h1>{userData?.data?.account?.username}</h1>
-          <p> {userData?.data?.firstName}</p>
-          <p> {userData?.data?.lastName}</p>
+              <h1>{userData?.data?.account?.username}</h1>
+            </div>
+          </div>
+        </div>
+        <div className="profilepage-body">
+          <div className="profilepage-info">
+            <h4>Profile Details</h4>
+            <div className="data">
+              <div className="Bio">
+                <span>
+                  <FaUserAlt className="Bio-icon" />
+                </span>{" "}
+                {userData?.data?.bio}
+              </div>
+              <div className="DOB">
+                {/* <strong>Date of Birth:</strong> */}
+                <span className="birthday-icon">
+                  <FaBirthdayCake />
+                </span>{" "}
+                {new Date(userData?.data?.dob).toLocaleDateString()}
+              </div>
+              <div className="Location">
+                <span className="location-icon">
+                  <FaLocationDot />{" "}
+                </span>
+                {userData?.data?.location}
+              </div>
+              <div className="Phonenumber">
+                <span className="phone">
+                  <IoCall />{" "}
+                </span>
+                {userData?.data?.countryCode} {userData?.data?.phoneNumber}
+              </div>
+            </div>
+          </div>
+
+          <Userpost
+            className="Getmyposts"
+            onUpdate={() => fetchMyPosts()}
+            posts={userNamepost}
+          />
         </div>
       </div>
-
-      <div className="profile-info">
-        <div className="data">
-          <p className="Bio">
-            <FaUserAlt /> {userData?.data?.bio}
-          </p>
-          <p className="DOB">
-            <FaBirthdayCake />{" "}
-            {new Date(userData?.data?.dob).toLocaleDateString()}
-          </p>
-          <p className="Location">
-            <FaLocationDot /> {userData?.data?.location}
-          </p>
-          <p className="Phonenumber">
-            <IoCall /> {userData?.data?.countryCode}{" "}
-            {userData?.data?.phoneNumber}
-          </p>
-        </div>
-      </div>
-
-      <Userpost posts={userNamepost} className="GetuserprofilebyUsername" />
-    </div>
+    </>
   );
 };
 
