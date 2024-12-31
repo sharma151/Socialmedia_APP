00bbb3c250174b2e78bc88b35690326eace9541c
sharma151 <sharmasaurav150503@gmail.com>
2024-12-31 15:35:07 +0545

    errorpagegadded

---
+++ src/Pages/Usernotfound.jsx
@@ -0,0 +1,25 @@
+import "../Styles/Sass/Pages/Usernotfound.scss";
+import { Link } from "react-router-dom";
+
+const Usernotfound = () => {
+  return (
+    <div className="user-not-found">
+      <div className="content">
+        <img
+          src="../src/assets/usernotfound.png"
+          alt="User Not Found"
+          className="image"
+        />
+        <h1>Oops! User Not Found</h1>
+        <p>
+          The user you're looking for doesn't exist or may have been removed.
+        </p>
+        <Link to="/" className="home-link">
+          Go Back to Homepage
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Usernotfound;
