591c32e320a6b5bd8d6a5afb6599c03bb33c6f34
sharma151 <sharmasaurav150503@gmail.com>
2025-05-20 17:57:06 +0545

    commented out

--- src/assets/Loader.jsx
+++ src/assets/Loader.jsx
@@ -1,20 +1,20 @@
-import { BallTriangle } from "react-loader-spinner";
+// import { BallTriangle } from "react-loader-spinner";
 
-const Loader = ({ className }) => {
-  return (
-    <div className={className}>
-      <BallTriangle
-        height={70}
-        width={70}
-        radius={5}
-        color="#243642"
-        ariaLabel="ball-triangle-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
-    </div>
-  );
-};
+// const Loader = ({ className }) => {
+//   return (
+//     <div className={className}>
+//       <BallTriangle
+//         height={70}
+//         width={70}
+//         radius={5}
+//         color="#243642"
+//         ariaLabel="ball-triangle-loading"
+//         wrapperStyle={{}}
+//         wrapperClass=""
+//         visible={true}
+//       />
+//     </div>
+//   );
+// };
 
-export default Loader;
+// export default Loader;
