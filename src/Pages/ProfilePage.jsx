c9fc40d19fb968eea41fdcf7282e4dbe64ccf48d
sharma151 <sharmasaurav150503@gmail.com>
2025-07-04 22:36:13 +0545

    tanstack implemented

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,91 +1,28 @@
-import { handleFetchmyPost, handleFetchMyprofile } from "../services/Handleapi";
-import { useState, useEffect } from "react";
-import { UpdatedataContext } from "@/Context/UpdateProfileContext";
+import { LoggedinUserProfileData } from "@/Context/UpdateProfileContext";
 import { useContext } from "react";
-import { toast } from "react-toastify";
-import ProfileUpdate from "@/Components/ProfileUpdate";
 import Createpost from "@/Components/Createpost";
 import Userpost from "@/Components/Userpost";
 import ProfileDetail from "../Components/Profiledetail";
 import CoverImage from "@/Components/CoverImage";
 import ProfileAvatar from "@/Components/ProfileAvatar";
-import Modal from "@/Modals/Modal";
+import { useMyPosts, useMyProfile } from "@/core/Hooks/Api/userData";
 
 const ProfilePage = () => {
-  const { UserprofileData } = useContext(UpdatedataContext);
-  const [profileImages, setprofileImages] = useState({
-    avatar: "",
-    coverImage: "",
-  });
-
-  const [errorMessage, setErrorMessage] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  // const closeModal = () => setShowModal(false);
-  const [loading, setLoading] = useState(false);
-  const [posts, setPosts] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const fetchProfile = async () => {
-    try {
-      const response = await handleFetchMyprofile();
-      setprofileImages(response);
-    } catch (error) {
-      toast.error("Error fetching profile:", error);
-      setErrorMessage("Error fetching profile data.");
-    }
-  };
-
-  const fetchMyPosts = async () => {
-    try {
-      const response = await handleFetchmyPost();
-      const sortedPosts = response.sort(
-        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-      );
-      setPosts(sortedPosts);
-    } catch (err) {
-      toast("Failed to fetch posts.", err);
-    }
-  };
-
-  const fetchAllData = async () => {
-    setLoading(true);
-    await Promise.all([fetchProfile(), fetchMyPosts()]);
-  };
-
-  useEffect(() => {
-    fetchAllData();
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setSelectedPost(null);
-  };
+  // const { loggedinUserprofileData } = useContext(LoggedinUserProfileData);
+  // const { firstName, lastName } = loggedinUserprofileData || {};
+  const { data: myPosts, isLoading, isError, error } = useMyPosts();
+  const { data: myprofile, refetch: fetchProfile } = useMyProfile();
 
   return (
     <div className="ProfilePage">
-      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
-      {/* {loading ? (
-        <Loader className="Profilepage-loader" />
-      ) : ( */}
       <>
         <div className="bg-gray-100 dark:bg-gray-800 pb-6">
-          <CoverImage
-            coverImageUrl={profileImages?.coverImage?.url || ""}
-            onUpdate={() => fetchProfile()}
-          />
+          <CoverImage coverImageUrl={myprofile?.coverImage?.url} />
           <div className="relative max-w-6xl mx-auto -mt-20 px-4 sm:px-6 lg:px-8">
             <ProfileAvatar
-              avatarUrl={profileImages?.account?.avatar?.url || ""}
-              name={`${UserprofileData?.firstName || ""} ${
-                UserprofileData?.lastName || ""
-              }`}
-              username={UserprofileData?.account?.username || ""}
-              onUpdate={() => fetchProfile()}
+              avatarUrl={myprofile?.account?.avatar?.url}
+              name={`${myprofile?.firstName || ""} ${myprofile?.lastName || ""}`}
+              username={myprofile?.account?.username || ""}
             />
           </div>
         </div>
@@ -94,10 +31,7 @@ const ProfilePage = () => {
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 mt-6 grid grid-cols-1 lg:grid-cols-3 gap-6">
           {/* Left - Profile Detail */}
           <div className="col-span-1">
-            <ProfileDetail
-              setIsModalOpen={setIsModalOpen}
-              UserprofileData={UserprofileData}
-            />
+            <ProfileDetail UserprofileData={myprofile} />
           </div>
 
           {/* Right - Create Post & Posts */}
@@ -109,22 +43,11 @@ const ProfilePage = () => {
             <Userpost
               className="Getmyposts"
               onUpdate={() => fetchMyPosts()}
-              posts={posts}
+              posts={myPosts}
             />
           </div>
         </div>
-
-        {/* Modal for profile update */}
-        {/* {showModal && ( */}
-        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-          <ProfileUpdate
-            closeModal={showModal}
-            onUpdate={() => fetchProfile()}
-          />
-        </Modal>
-        {/* )} */}
       </>
-      {/* )} */}
     </div>
   );
 };
